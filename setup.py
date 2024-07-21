3f11cc46c5e8d15be428513fda77aefa6c6be342
pankajkulria-1 <pankajkulria.iitkgp@gmail.com>
2024-07-21 12:51:36 +0530

    Requirements installed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name="us_Visa",
+    version="0.0.0.0",
+    author="Pankaj",
+    author_email="pankajkulria.iitkgp@gmail.com",
+    packages=find_packages()
+
+)
